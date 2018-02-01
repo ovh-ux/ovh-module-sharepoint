8f1833cc520dfd5c5a9c416c4ae3a17630c69995
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2018-02-01 16:18:28 -0500

    feat(sharepoint): oui-datagrid tasks

--- src/sharepoint/task/sharepoint-task.controller.js
+++ src/sharepoint/task/sharepoint-task.controller.js
@@ -10,45 +10,21 @@ angular
         }
 
         $onInit () {
-            this.$scope.taskDetails = [];
-            this.hasResult = false;
-            this.loaders = {
-                tasks: true,
-                pager: false
-            };
-
             this.getTasks();
         }
 
         getTasks () {
-            this.loaders.tasks = true;
             this.tasksIds = null;
 
             return this.sharepointService.getTasks(this.$stateParams.exchangeId)
-                .then((ids) => {
-                    this.tasksIds = ids;
-                    if (_.isArray(ids) && !_.isEmpty(ids)) {
-                        this.hasResult = true;
-                    }
-                })
+                .then((ids) => { this.tasksIds = ids.map((id) => ({ id })); })
                 .catch((err) => {
                     _.set(err, "type", err.type || "ERROR");
                     this.alerter.alertFromSWS(this.$scope.tr("sharepoint_tabs_tasks_error"), err, this.$scope.alerts.main);
-                })
-                .finally(() => {
-                    if (_.isEmpty(this.tasksIds)) {
-                        this.hasResult = false;
-                        this.loaders.tasks = false;
-                    }
                 });
         }
 
         onTransformItem (taskId) {
             return this.sharepointService.getTask(this.$stateParams.exchangeId, taskId);
-        }
-
-        onTransformItemDone () {
-            this.loaders.tasks = false;
-            this.loaders.pager = false;
         }
     });
