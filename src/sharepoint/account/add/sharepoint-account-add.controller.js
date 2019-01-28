eeebfc1ce797255bc9e2ac51c9251847732a6223
Jérôme Martin <jerome.martin@corp.ovh.com>
2019-01-28 14:43:59 +0100

    fix(order): require the latest ui-kit

--- src/sharepoint/account/add/sharepoint-account-add.controller.js
+++ src/sharepoint/account/add/sharepoint-account-add.controller.js
@@ -56,8 +56,10 @@ angular
 
     submit() {
       const getOrderUrl = this.isProvider
-        ? this.sharepointLicense.getSharepointProviderNewAccountOrderUrl.bind(this.sharepointLicense)
-        : this.sharepointLicense.getSharepointStandaloneNewAccountOrderUrl.bind(this.sharepointLicense);
+        ? this.sharepointLicense.getSharepointProviderNewAccountOrderUrl
+          .bind(this.sharepointLicense)
+        : this.sharepointLicense.getSharepointStandaloneNewAccountOrderUrl
+          .bind(this.sharepointLicense);
 
       this.alerter.success(this.$translate.instant('sharepoint_account_action_sharepoint_add_success_message'), this.$scope.alerts.main);
       this.$scope.resetAction();
